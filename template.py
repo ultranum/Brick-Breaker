70e3ba808ec800179e8d81ad453ce6c53020b267
Garrett Chen <g.chen4@share.epsb.ca>
2019-05-01 11:56:18 -0600

    brciks

--- template.py
+++ template.py
@@ -28,11 +28,7 @@ screen.fill(GREY) # Fills the entire surface with the color. Think of fill as er
 clock = pygame.time.Clock() # Starts a clock to measure time
 
 class blocks:
-    pass
-
-
-class player:
-    def __init__(self, x = 0, y = 0, width = 10, height = 10):
+    def __init__(self, x=0, y=0, width=50, height=10):
         self.x = x
         self.y = y
         self.pos = (self.x, self.y)
@@ -42,7 +38,7 @@ class player:
         self.xDir = 1
         self.yDir = -1
         self.color = WHITE
-        self.surface = pygame.Surface((0,0), pygame.SRCALPHA, 32)
+        self.surface = pygame.Surface((0, 0), pygame.SRCALPHA, 32)
         self.surface.fill(self.color)
 
     def getSurface(self):
@@ -58,23 +54,30 @@ class player:
         self.surface = pygame.Surface(self.dim, pygame.SRCALPHA, 32)
         self.surface.fill(self.color)
 
-class paddle(player):
-    def __init__(self):
-        player.__init__(self)
+    def collision(self):
+        pass
 
+    def __repr__(self):
+        return ('%s, %s'%(self.x, self.y))
+class paddle(blocks):
+
+    def __init__(self, x, y, width, height):
+        blocks.__init__(self, x, y, width, height)
+        self.surface = pygame.Surface(self.dim, pygame.SRCALPHA, 32)
+        self.surface.fill(self.color)
     def playerMove(self, pressedKey, spd=5):
-        if pressedKey[pygame.K_w]:
-            self.y -= spd
-        if pressedKey[pygame.K_s]:
-            self.y += spd
+
         if pressedKey[pygame.K_a]:
             self.x -= spd
         if pressedKey[pygame.K_d]:
             self.x += spd
+        self.pos = (self.x, self.y)
 
-class ball(player):
+
+
+class ball(blocks):
     def __init__(self, x, y, width, height):
-        player.__init__(self, x, y, width, height)
+        blocks.__init__(self, x, y, width, height)
 
     def autoMove(self, spex = 10, spey = 10):
         self.x += self.xDir * spex
@@ -91,19 +94,40 @@ class ball(player):
             self.yDir = 1
         self.pos = (self.x, self.y)
 
-
+def getSpriteCollision(sprite1, sprite2):
+    if sprite2.getX() <= sprite1.getX() + sprite1.getWidth() <= sprite2.getX() + sprite2.getWidth() + sprite1.getWidth() and sprite2.getY() <= sprite1.getY() + sprite1.getHeight() <= sprite2.getY() + sprite2.getHeight() + sprite1.getHeight():
+        return True
+    else:
+        return False
 # --- CODE STARTS HERE --- #
-
-ball = ball(50, 50, 50, 50)
+paddle = paddle(90, 90, 100, 20)
+ball = ball(20, 20, 20, 20)
 ball.setPos(30,30)
+paddle.setPos(WIDTH/2,HEIGHT - 30)
+blockList = []
+blockList.append(blocks(0,0))
+blockList[0].setPos(0,0)
+for i in range(10):
+    x = blockList[i-1].x + blockList[i-1].width + 10
+    y = 0
+    blockList.append(blocks(x, y))
+    blockList[i].setPos(x, y)
+    if (blockList[i].x + blockList[i].width) >= WIDTH:
+        y = blockList[i].height + blockList[i].y + 2
+print(blockList)
 running = True
 while running:
     for event in pygame.event.get(): # returns all inputs and triggers into an array
         if event.type == pygame.QUIT: # If the red X was clicked.
             running = False
+        pressedKeys = pygame.key.get_pressed()
     ball.autoMove()
+    paddle.playerMove(pressedKeys, 10)
     screen.fill(GREY)
+    screen.blit(paddle.getSurface(), paddle.getPOS())
     screen.blit(ball.getSurface(), ball.getPOS())
+    for i in range(len(blockList)):
+        screen.blit(blockList[i].getSurface(), blockList[i].getPOS())
     clock.tick(FPS) # pause the game until the FPS time is reached
     pygame.display.flip() # update the screen with changes.
 pygame.quit()
